515f9e669a3bacdaf56ef8e025dbfd1ee6aaddce
JPastor13 <Richard.pastor.sanchez@gmail.com>
2024-08-10 23:21:58 -0500

    primera actualización

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,35 @@
+import { Box, Flex, HStack, Image, VStack,Text,Heading,Button} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <HStack border='solid red 1px' height='308px' justifyContent='center'>
+     <VStack height='250px' width='359px' border='solid red 1px'>
+     
+     <HStack>
+     <Text fontSize='32px' fontWeight='bold'>CONTACTO</Text>
+     <Image src="./src/img/wsp.png" alt="whasap"></Image>
+     </HStack>
+     <VStack>
+      <Text> Dirección: Jr. Manuel Villavicencio 472</Text>
+      <Text> Chimbote,Peru</Text>
+      <Text>Correo: info@plazasantander.com.pe</Text>
+      <Button height='60px' width='320px' colorScheme='black' variant='outline'>COMO LLEGAR</Button>
+    </VStack>
+     </VStack>
+     <VStack height='250px' width='359px' border='solid red 1px'>
+     <Text fontSize='32px' fontWeight='bold'>HORARIO</Text>
+     <Text></Text>
+     </VStack>
+     <VStack height='250px' width='359px' border='solid red 1px'>
+     <Text fontSize='32px' fontWeight='bold'>SIGUENOS</Text>
+
+
+     </VStack>
+
+    </HStack>
+
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
-import { Box, HStack, Image, VStack } from '@chakra-ui/react'
+import { Box, Flex, HStack, Image, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <HStack bgColor={{ base: 'blue', md: 'yellow', lg: 'red' }}
-        height='120px' justifyContent='space-between'>
+        <HStack bgColor={{ base: '#F5F5F5', md: '#F5F5F5', lg: '#F5F5F5' }}
+        height='151px' justifyContent='space-between'>
             {/* logo de la plaza */}
-            <Image src="" alt="logo de la plaza" />
+            <Image height='150px' width='217' src="./src/img/logo.png" alt="logo de la plaza"  />
 
             <VStack>
-                <Box>hola</Box>
-                <HStack textTransform='uppercase'>
+
+                <HStack  border='solid red 1px'>
+                <Link  to='/tiendas' >Alquiler</Link>
+                <Link to='/tiendas'>Plano del centro comercial</Link>
+                <Link to='/tiendas'>Comó llegar</Link>
+                <Link to='/tiendas'>Contactos</Link>
+                </HStack>
+                <HStack gap='20px' fontSize='20px' textTransform='uppercase'>
                     <Link to='/tiendas'>Tiendas</Link>
+                    <Link to='/productos'>Entrenamientos</Link>
+                    <Link to='/productos'>Productos</Link>
                     <Link to='/productos'>Productos</Link>
                 </HStack>
             </VStack>
         </HStack>
+
     )
 }
 
